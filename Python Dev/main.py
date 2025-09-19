6e370bf22831c366e7b3351253c04997f047d891
Daniel <Daniel.VSobralV3@gmail.com>
2025-09-19 14:11:29 -0300

    new: file main.py first step

---
+++ Python Dev/main.py
@@ -0,0 +1,25 @@
+#Titulo 
+#Input do Chat
+#A cada mensagem enviada:
+  # Mostrar a mensagem que o usuario enviou no chat
+  # Enviar essa mensagem  para IA responder 
+  # Aparace na tel a resposta da IA
+
+# Streamlit - FrontEnd e BackEnd
+
+import streamlit as st
+
+st.write("## ChatBot com IA")
+
+msg_usuario = st.chat_input("Escreva sua mensagem aqui")
+
+if msg_usuario:
+  print(msg_usuario)
+  st.chat_message("Daniel").write(msg_usuario)
+
+  msg_ia = "Você quis dizer:" + msg_usuario +"?"
+
+  st.chat_message("assistant").write(msg_ia)
+
+  #User -> serhumano
+  #assistant - > Inteligencia artificial
